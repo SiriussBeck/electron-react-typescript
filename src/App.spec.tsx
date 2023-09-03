9fd4c5c3f42b97a1d191a4cf3eb14ee723e42c20
pySiriusDev <siriusdev@outlook.com>
2023-09-03 12:18:52 -0300

    remove jest

--- src/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer'
-import App from './App'
-
-it('Renders the "Hello World" page', () => {
-  const page = renderer.create(<App />)
-  const tree = page.toJSON()
-  expect(tree).toMatchSnapshot()
-})
